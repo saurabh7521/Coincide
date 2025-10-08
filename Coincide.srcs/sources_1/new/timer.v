0043ee6269653ad9026373bb14a62912ac3958b3
saurabh7521 <52254104+saurabh7521@users.noreply.github.com>
2025-10-07 20:50:45 -0400

    Update timer.v

--- Coincide.srcs/sources_1/new/timer.v
+++ Coincide.srcs/sources_1/new/timer.v
@@ -5,7 +5,7 @@
 module timer #(
     parameter SIZE = 32, // Width of the counter
     parameter DIV = 0,  // Number of bits to use as divisor
-    parameter TOP = 100_000_000,// Max value, 0 means no maximum
+    //parameter TOP = 300_000_000,// Max value, 0 means no maximum
     parameter UP = 1    // Direction to count, 1 for up and 0 for down
 )(
     input wire clk,     // Clock input
@@ -16,9 +16,11 @@ module timer #(
 
     // Internal counter register, width is SIZE + DIV
     reg [SIZE+DIV-1:0] ctr;
+    reg done;
    
     // Define the maximum value based on parameters
-    localparam [SIZE+DIV-1:0] MAX_VALUE = 100_000_000; // Value when maxed out
+    localparam [SIZE+DIV-1:0] MAX_VALUE = 30_000_000; // Value when maxed out
+
 
     // Counter logic
     always @(posedge clk or posedge rst) begin
@@ -26,25 +28,20 @@ module timer #(
             ctr <= 0;         // Reset internal counter
             value <= 0;       // Reset output value
             maxval <= 0;      // Reset maxval output
+            done   <= 0;
         end else begin
-            maxval <= 0; // Default maxval to LOW
-            
+            done <= (ctr == MAX_VALUE - 1);  // <-- pre-trigger
+            maxval <= 0; // Default maxval to LOW            
             if (UP) begin // Up counter
-                if (ctr == MAX_VALUE) begin // Check if max value reached
+                //done <= (ctr == MAX_VALUE);
+                if (done) begin // Check if max value reached
                     ctr <= 0;    // Reset counter
                     maxval <= 1; // Set maxval HIGH
                 end else begin
                     ctr <= ctr + 1; // Increment counter
+                    
                 end
-            end else begin // Down counter
-                if (ctr == 0) begin // Check if counter reached zero
-                    ctr <= MAX_VALUE; // Reset to max value
-                    maxval <= 1; // Set maxval HIGH
-                end else begin
-                    ctr <= ctr - 1; // Decrement counter
-                end
-            end
-            
+            end    
             value <= ctr; // Set the output value
         end
     end
