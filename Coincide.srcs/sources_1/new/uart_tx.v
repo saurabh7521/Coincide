96dc6881abef6241295a1ab0f79fffc955237949
saurabh7521 <52254104+saurabh7521@users.noreply.github.com>
2025-10-07 20:52:50 -0400

    Update uart_tx.v
    Implemented handshaking protocol between the FPGA and the PC to eliminate jitter.

--- Coincide.srcs/sources_1/new/uart_tx.v
+++ Coincide.srcs/sources_1/new/uart_tx.v
@@ -1,6 +1,6 @@
 module uart_tx #(
-    parameter CLK_FREQ = 100_000_000,  // clock frequency
-    parameter BAUD = 9600         // desired baud rate
+    parameter CLK_FREQ = 300_000_000,  // clock frequency
+    parameter BAUD = 14400         // desired baud rate
 )(
     input clk,            // clock input
     input rst,            // reset active high
